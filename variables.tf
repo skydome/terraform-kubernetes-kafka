7e03a7e1b0194397dfe8b09fa49a94d7afd92d77
canavar <abdulkadiryaman@gmail.com>
2019-09-06 18:43:39 +0300

    default variables

--- variables.tf
+++ variables.tf
@@ -1,12 +1,21 @@
 variable "namespace" {}
 variable "kafka_name" {}
 variable "cluster_size" {}
-variable "offset_topic_replication_factor" {}
-variable "zookeeper_cluster_size" {}
-variable "kafka_rest_replicacount" {}
-variable "kafka_rest_ingress_host" {}
+variable "offset_topic_replication_factor" {
+  default = 1
+}
+variable "zookeeper_cluster_size" {
+  default = 1
+}
+variable "kafka_rest_replicacount" {
+  default = 1
+}
+variable "kafka_rest_ingress_host" {
+  default = "mydearhost.io"
+}
 variable "kafka_rest_ingress_annotations" {
   type = list(object({key = string, value = string}))
+  default = []
 }
 variable "kafka_rest_enabled" {
   type = bool
